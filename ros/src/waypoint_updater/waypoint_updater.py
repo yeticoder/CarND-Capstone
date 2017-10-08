c9de57a4411b1b511789fe5cc71fb551c8fef368
Shubham Chopra <chopra.shubham@gmail.com>
2017-10-08 09:24:41 -0400

    Updating Pillow version. Adding waypoint_updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,20 +33,59 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
-
+        self._base_waypoints = None
+        self._current_pose = None
+        self._last_waypoint = None
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self._current_pose = msg
+        # we now need to find the waypoint closest and in-front of this waypoint and add
+        # waypoint after that to future waypoints, which in turn is published
+        future_waypoints = []
+        min_wp = None
+        if self._last_waypoint is None:
+            # we are starting. so look through the entire base-waypoints list
+            min_wp = self.minDistanceWayPoint(0, len(self._base_waypoints), self._current_pose)
+        else:
+            # only look through LOOKAHEAD_WPS from the past location
+            # assuming that the car won't run through all the way points
+            # between 2 successive calls to this function
+            min_wp = self.minDistanceWayPoint(self._last_waypoint, self._last_waypoint + LOOKAHEAD_WPS, self._current_pose)
+        # rospy.loginfo('Waypoint: {0}'.format(min_wp))
+        # rospy.loginfo('Waypoint: {0}'.format(self._base_waypoints[min_wp]))
+        # rospy.loginfo('Current position: {0}'.format(msg.pose))
+        for i in range(min_wp, min_wp + LOOKAHEAD_WPS):
+            future_waypoints.append(self._base_waypoints[i % len(self._base_waypoints)])
+        self._last_waypoint = min_wp
+        pubMsg = Lane()
+        pubMsg.header.frame_id = '/world'
+        pubMsg.header.stamp = rospy.Time(0)
+        pubMsg.waypoints = future_waypoints
+        self.final_waypoints_pub.publish(pubMsg)
+
+    def minDistanceWayPoint(self, start, end, pose):
+        min_wp = start
+        min_dist = self.distancePose(pose, self._base_waypoints[start].pose)
+        base_len = len(self._base_waypoints)
+        for i in range(start + 1, end):
+            dist = self.distancePose(pose, self._base_waypoints[i % base_len].pose)
+            if(dist < min_dist):
+                min_dist = dist
+                min_wp = i % base_len
+        return min_wp
+
+    def distancePose(self, pose1, pose2):
+        x = pose1.pose.position.x - pose2.pose.position.x
+        y = pose1.pose.position.y - pose2.pose.position.y
+        z = pose1.pose.position.z - pose2.pose.position.z
+        return math.sqrt(x**2 + y**2 + z**2)
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self._base_waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
