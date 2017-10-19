f1548536a2180a30c9d3f541a5c18f2723324249
Hidde Stokvis <hidde@neverhide.nl>
2017-10-19 17:11:45 +0200

    Change: changed log info for better insights

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -80,8 +80,8 @@ class DBWNode(object):
         self.controller = Controller(info)
 
         # Subscribe to all the topics you need to
-        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb, queue_size=5)
-        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb, queue_size=5)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb, queue_size=1)
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb, queue_size=1)
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb, queue_size=1)
 
         self.loop()
@@ -127,7 +127,7 @@ class DBWNode(object):
                     current_velocity = self.current_velocity,
                     delta_time = delta_time
                 )
-                rospy.loginfo("THROTTLE: %s, BRAKE: %s, STEER: %s", throttle, brake, steer)
+                rospy.loginfo("CURRENT: %s, TARGET: %s", self.current_velocity.twist.linear.x, self.twist_cmd.twist.linear.x)
                 self.publish(throttle, brake, steer)
             else:
                 self.reset = True
